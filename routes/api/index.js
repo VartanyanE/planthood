3badf6e333ed596741644e1d7fddd924187d3df0
mikevampm323 <mike.vartanyan@yahoo.com>
2020-05-31 14:52:59 -0700

    modified one key in user model, added login route, wrote strategy for signup/login

---
+++ routes/api/index.js
@@ -0,0 +1,16 @@
+var db = require("../models");
+var passport = require("../../config/passport");
+
+module.exports = function (app) {
+  // Using the passport.authenticate middleware with our local strategy.
+  // If the user has valid login credentials, send them to the members page.
+  // Otherwise the user will be sent an error
+  app.post(
+    "/api/login",
+    passport.authenticate("local.signup", {
+      successRedirect: "/profile",
+      failureRedirect: "/signup",
+      failureFlash: true,
+    })
+  );
+};
