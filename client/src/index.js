2966b21556f12cb0d93bac5ab9d774b3c7412474
Anna Olt <anna.olt16@gmail.com>
2020-06-03 19:41:28 -0400

    I made the Navbar green

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { ThemeProvider } from "@material-ui/core";
+import { createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#228B22",
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Container } from "@material-ui/core";
+import { Button, Container, ThemeProvider } from "@material-ui/core";
 
 function Landing() {
   return (
@@ -7,8 +7,12 @@ function Landing() {
       <Container>
         <img src={"logo.png"} />
         <h1>Landing</h1>
-        <Button variant="outlined">Sign Up</Button>
-        <Button variant="outlined">Log In</Button>
+        <Button color="primary" variant="outlined">
+          Sign Up
+        </Button>
+        <Button color="primary" variant="outlined">
+          Log In
+        </Button>
       </Container>
     </div>
   );
